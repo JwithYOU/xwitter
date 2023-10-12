e7b9a5c8eb2f7e004031d298f9eef0cee94f806e
J with YOU <khy960101@gmail.com>
2023-10-12 10:32:51 +0900

    fire store 연동

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
 
 const Home = () => {
-  return <span>Home</span>;
+  const [xweet, setXweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("xweets").add({
+      xweet,
+      createdAt: Date.now(),
+    });
+    setXweet("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setXweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={xweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Xweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
