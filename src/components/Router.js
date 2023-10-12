add24b9e25198054f110f62961bd15cd9f2072f8
J with YOU <khy960101@gmail.com>
2023-10-12 09:40:34 +0900

    route 이동 문제 해결

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLogginIn }) => {
   return (
     <Router>
+      {isLogginIn && <Navigation />}
       <Switch>
         {isLogginIn ? (
           <>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
             </Route>
           </>
         ) : (
